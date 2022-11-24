34ec01139498444f7843cee262f9034566db694c
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-11-24 20:49:20 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,25 +1,40 @@
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
+import movieModel from './movieModel';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); 
-router.get('/', (req, res) => {
-    res.json(movies);
-});
+
+router.get('/', asyncHandler(async (req, res) => {
+    const movies = await movieModel.find();
+    res.status(200).json(movies);
+}));
 
 // Get movie details
-router.get('/:id/reviews', (req, res) => {
+router.get('/:id', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
-    // find reviews in list
-    if (movieReviews.id == id) {
-        res.status(200).json(movieReviews);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
     } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
+        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
-});
+}));
+
+// // Get movie details
+// router.get('/:id/reviews', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     // find reviews in list
+//     if (movieReviews.id == id) {
+//         res.status(200).json(movieReviews);
+//     } else {
+//         res.status(404).json({
+//             message: 'The resource you requested could not be found.',
+//             status_code: 404
+//         });
+//     }
+// });
 
 //Post a movie review
 router.post('/:id/reviews', (req, res) => {
