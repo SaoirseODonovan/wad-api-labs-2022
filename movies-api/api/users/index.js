154ddaf12c5f06351565023575f7a6b67468a6ad
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-11-24 21:08:49 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -18,10 +18,16 @@ router.post('/',asyncHandler( async (req, res, next) => {
       res.status(401).json({success: false, msg: 'Please pass username and password.'});
       return next();
     }
+    //added for exercises
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
-    } else {
+        if(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/.test(req.body.password)) {
+            await User.create(req.body).catch(next);
+        } else {
+            res.status(401).json({code: 401,msg: 'Authentication failed. Password to weak'});
+        }
+        res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      } 
+      else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
         user.comparePassword(req.body.password, (err, isMatch) => {
@@ -56,7 +62,9 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
-    await user.favourites.push(movie._id);
+    if(await user.favourites.includes(movie.id)){
+        await user.favourites.push(movie._id);
+    }
     await user.save(); 
     res.status(201).json(user); 
   }));
