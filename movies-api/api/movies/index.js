ef73a817e0d6ce9d65b06bb8f155e7ea86dbbcb5
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-11-25 12:20:42 +0000

    Topic 11 - Set up

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -12,14 +12,19 @@ router.get('/', asyncHandler(async (req, res) => {
 }));
 
 // Get movie details
-router.get('/:id', asyncHandler(async (req, res) => {
-    const id = parseInt(req.params.id);
-    const movie = await movieModel.findByMovieDBId(id);
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
-    }
+router.get('/', asyncHandler(async (req, res) => {
+    let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
+    [page, limit] = [+page, +limit]; //trick to convert to numeric (req.query will contain string values)
+
+    const totalDocumentsPromise = movieModel.estimatedDocumentCount(); //Kick off async calls
+    const moviesPromise = movieModel.find().limit(limit).skip((page - 1) * limit);
+
+    const totalDocuments = await totalDocumentsPromise; //wait for the above promises to be fulfilled
+    const movies = await moviesPromise;
+
+    const returnObject = { page: page, total_pages: Math.ceil(totalDocuments / limit), total_results: totalDocuments, results: movies };//construct return Object and insert into response object
+
+    res.status(200).json(returnObject);
 }));
 
 // // Get movie details
